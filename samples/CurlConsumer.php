b5bd684e42d4b38e4cb84cf2e043871daa94a247
Bhumil Haria <bhumil@kratee.com>
2017-04-05 06:22:53 +0000

    Update sample code to reflect compatibility with autoload

--- samples/CurlConsumer.php
+++ samples/CurlConsumer.php
@@ -1,8 +1,11 @@
 <?php
 
-// import the DnifLogger and Consumers_BlockingHttpConsumer classes
-require_once("../lib/Consumers.php");
-require_once("../lib/DnifLogger.php");
+// autoload will import the necessary classes
+require_once("../vendor/autoload.php");
+
+// loading individually: DnifLogger and Consumers_BlockingHttpConsumer classes
+// require_once("../vendor/dnif/php-logger/lib/Consumers.php");
+// require_once("../vendor/dnif/php-logger/lib/DnifLogger.php");
 
 $dlog = new DnifLogger(
     new Consumers_BlockingHttpConsumer("http://TARGET_IP:PORT/json/receive"));
